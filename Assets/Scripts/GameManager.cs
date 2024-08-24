ecb5b95c1306a88f0a5be21f038f35be9171e487
Bora Cingoz <boracingoz@gmail.com>
2024-08-24 12:06:39 +0300

    GameManager and stand algorithsm added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject selectedCircle;
+    public GameObject selectedGround;
+    Circle circle;
+    public bool isMove;
+
+
+    public int targetStandNumber;
+    public int currentStandNumber; //tamamlanan stand sayýsý.
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+            {
+                if (hit.collider != null && hit.collider.CompareTag("Stand"))
+                {
+                    if (selectedCircle !=null && selectedCircle != hit.collider.gameObject)
+                    {//çemberi gönder. 
+                        
+                    }
+                    else
+                    {
+                        Stand stand = hit.collider.GetComponent<Stand>();
+                        selectedCircle = stand.TopMostCircle();
+                        circle = selectedCircle.GetComponent<Circle>();
+                        isMove = true;
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Stand.cs
+++ Assets/Scripts/Stand.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Stand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject movePos;
+    public GameObject[] sockets;
+    public List<GameObject> circles = new();
+    [SerializeField] private GameManager _gameManager;
+   
 
-    // Update is called once per frame
-    void Update()
+    public GameObject TopMostCircle()
     {
-        
+        return circles[^1];
     }
 }
